2f8f55932ba9f868e67b4e50e6a54a2ffd348c9a
elliot <1094253525@qq.com>
2022-07-06 12:48:50 +0800

    重新构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./",
+  },
+  base: `/c_firework`,
   plugins: [
     vue(),
     eslintPlugin({
-      include: ['src/**/*.js','src/**/*.vue','src/*.js', 'src/*.vue']
-    })
-  ]
-})
+      include: ["src/**/*.js", "src/**/*.vue", "src/*.js", "src/*.vue"],
+    }),
+  ],
+});
